644b2a342f11f1122e339ea55fb9d711d2e78da3
zhengqingquan <55186872+zhengqingquan@users.noreply.github.com>
2021-08-20 08:07:47 +0800

    change

--- main/ExtMerchant.lua
+++ main/ExtMerchant.lua
@@ -1,6 +1,12 @@
+--[[
+插件原为：Extended Vendor UI
+原作者为：Germbread
+--]]
+
+
 -- 全局变量
 EXTMERCHANT = {
-    ItemsPerSubpage = MERCHANT_ITEMS_PER_PAGE, --每个子页面的物品数量
+    ItemsPerSubpage = MERCHANT_ITEMS_PER_PAGE, --此处为10，表示左右两个页面为10。每个子页面的物品数量
 }
 
 -- overrides default value of base ui, default functions will handle page display accordingly
@@ -22,12 +28,13 @@ function ExtMerchant_OnLoad(self)
     hooksecurefunc("MerchantNextPageButton_OnClick", ExtMerchant_UpdateSlotPositions)
 
     -- 商品页面钩子函数
-    -- 上下翻页的时候也会触发MerchantFrame_UpdateMerchantInfo
-    -- 拾取物品也会触发
+    -- 上下翻页的时候也会触发
+    -- 拾取物品(非金币)也会触发
     hooksecurefunc("MerchantFrame_UpdateMerchantInfo", ExtMerchant_UpdateSlotPositions)
-    -- 回购页面钩子函数
-    hooksecurefunc("MerchantFrame_UpdateBuybackInfo", ExtMerchant_UpdateBuyBackSlotPositions)
 
+    -- 回购页面钩子函数
+    -- 回购页面的触发相对商品页面较为稳定，只有进入回购才触发。
+    hooksecurefunc("MerchantFrame_UpdateBuybackInfo", ExtMerchant_UpdateBuyBackSlotPositions)
 end
 
 
@@ -61,7 +68,7 @@ function ExtMerchant_RebuildMerchantFrame()
     MerchantFrame:SetWidth(690)
 
     -- create new item buttons as needed
-    -- 根据需要创建物品部件
+    -- 根据需要创建物品槽组件
     for i = 1, MERCHANT_ITEMS_PER_PAGE, 1 do
         if (not _G["MerchantItem" .. i]) then
             CreateFrame("Frame", "MerchantItem" .. i, MerchantFrame, "MerchantItemTemplate")
@@ -75,15 +82,12 @@ function ExtMerchant_RebuildMerchantFrame()
     MerchantNextPageButton:SetPoint("CENTER", MerchantFrame, "BOTTOM", 290, 55);
 
     -- alter the position of the buyback item slot on the merchant tab
-    -- 设置商店标签上物品回购槽的位置。在进行其他操作的时候位置可能会改变。
+    -- 更改当在商人标签时的回购槽的位置。
     -- 因为锚点是MerchantItem10所以位置会根据MerchantItem10偏移。
-    -- MerchantBuyBackItem的左上角被固定在了，MerchantItem10的左下角，在Y轴移动过了-53的像素点。
-    -- 可用修改为固定在整体界面的某个位置
     MerchantBuyBackItem:SetPoint("TOPLEFT", MerchantItem10, "BOTTOMLEFT", -14, -20)
     
     -- 隐藏游戏自带的过滤器
     -- MerchantFrameLootFilter:Hide();
-
 end
 
 --========================================
@@ -91,26 +95,24 @@ end
 -- 重新排列商品物品槽的位置。
 --========================================
 function ExtMerchant_UpdateSlotPositions()
-
     -- 购买垂直间距为-16，水平间距为12，单位：像素
     local vertSpacing= -16 -- 垂直间距
     local horizSpacing = 12 -- 水平间距
     local buy_slot -- 临时变量
     
-    -- 根据暴雪原生的UI，应该只要调整MerchantItem1的位置就可用改变后面11个的位置。
-    --try
-
-    -- 循环物品，稍微调整物品槽的位置。
+    -- 循环物品槽，调整物品槽的锚点和位置。
+    -- 参考：
+    -- https://wowpedia.fandom.com/wiki/API_Region_SetPoint
+    -- https://bbs.nga.cn/read.php?tid=4555096
+    -- https://www.townlong-yak.com/framexml/live/MerchantFrame.xml#89
     for i = 1, MERCHANT_ITEMS_PER_PAGE, 1 do
         buy_slot = _G["MerchantItem" .. i]
-
-        -- 在暴雪原生UI中，MerchantItem11和MerchantItem12被隐藏了。需要主动显示。
+        -- 在暴雪原生UI中，MerchantItem11和MerchantItem12被隐藏了。需要主动显示全部物品槽。
         buy_slot:Show()
 
-        -- ItemsPerSubpage1
         if ((i % EXTMERCHANT.ItemsPerSubpage) == 1) then
             if (i == 1) then
-                -- 按钮1
+                -- 物品槽1
                 buy_slot:SetPoint("TOPLEFT", MerchantFrame, "TOPLEFT", 24, -70)
             else
                 -- 第二排
@@ -118,7 +120,7 @@ function ExtMerchant_UpdateSlotPositions()
             end
         else
             if ((i % 2) == 1) then
-                -- 单数按钮，不包含1
+                -- 单数按钮，不包含物品槽1
                 buy_slot:SetPoint("TOPLEFT", _G["MerchantItem" .. (i - 2)], "BOTTOMLEFT", 0, vertSpacing)
             else
                 -- 双数按钮
@@ -138,17 +140,23 @@ function ExtMerchant_UpdateBuyBackSlotPositions()
     local horizSpacing = 50 -- 水平间距
     local buyback_slot -- 临时变量
 
+    -- 循环物品槽，调整物品槽的锚点和位置。
+    -- 参考：
+    -- https://wowpedia.fandom.com/wiki/API_Region_SetPoint
+    -- https://bbs.nga.cn/read.php?tid=4555096
+    -- https://www.townlong-yak.com/framexml/live/MerchantFrame.xml#89
     for i = 1, MERCHANT_ITEMS_PER_PAGE, 1 do
         buyback_slot = _G["MerchantItem" .. i]
 
+        -- BUYBACK_ITEMS_PER_PAGE默认为12
         if (i > BUYBACK_ITEMS_PER_PAGE) then
-            buyback_slot:Hide(); -- 隐藏
+            buyback_slot:Hide(); -- 多余的物品栏进行隐藏
         else
+            -- 回购槽1
             if (i == 1) then
-                -- 设置UI组件的附着点。
-                -- 参考https://wowpedia.fandom.com/wiki/API_Region_SetPoint
                 buyback_slot:SetPoint("TOPLEFT", MerchantFrame, "TOPLEFT", 64, -105);
             else
+                -- 下一排
                 if ((i % 3) == 1) then
                     buyback_slot:SetPoint("TOPLEFT", _G["MerchantItem" .. (i - 3)], "BOTTOMLEFT", 0, vertSpacing);
                 else
@@ -157,64 +165,4 @@ function ExtMerchant_UpdateBuyBackSlotPositions()
             end
         end
     end
-end
-
-
---========================================
--- Show and update merchant page
--- 展示并更新商人页面
---========================================
-function ExtMerchant_UpdateMerchantInfo(isPageScroll)
-
-end
-
-
---========================================
--- Slash command handler
--- 斜杠命令处理程序
---========================================
--- 目前用来测试某个功能。
-function ExtMerchant_CommandHandler()
-
-    -- 输入斜杠命令后更新商品界面
-    ExtMerchant_UpdateSlotPositions()
-
-
-end
-
-
---========================================
--- Previous page button handler
--- (also used for wheel scrolling)
--- 前一页按钮处理器（同样被用在滚轮滚动）
---========================================
-function ExtMerchant_PrevPageButton()
-    -- ExtMerchant_UpdateSlotPositions()
-    print("前一页被点击")
-end
-
---========================================
--- Next page button handler
--- (also used for wheel scrolling)
--- 下一页按钮处理器（同样被用在滚轮滚动）
---========================================
-function ExtMerchant_NextPageButton()
-    -- ExtMerchant_UpdateSlotPositions()
-    print("下一页被点击")
-end
-
---========================================
--- Hooked merchant frame OnShow
--- 钩住商人框体的OnShow方法
---========================================
-function ExtMerchant_OnShow()
-    -- 重新排列物品位置
-    -- ExtMerchant_UpdateSlotPositions()
-end
-
---========================================
--- Hooked merchant frame OnHide
--- 钩住商人框体的OnHide方法
---========================================
-function ExtMerchant_OnHide()
 end
